de81fdac5a6a6ed7529cd0aeacb86541b5344714
kinosita28 <hongyoumuxia49@gmail.com>
2021-02-16 20:00:36 +0900

    テストコード修正

--- spec/models/buy_spec.rb
+++ spec/models/buy_spec.rb
@@ -13,62 +13,62 @@ RSpec.describe Buy, type: :model do
       it "user_idがないと購入出来ない" do
         @buy.user_id = ""
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("User can't be blank")
+        expect(@buy.errors.full_messages).to include("ユーザーを入力してください")
       end  
       it "item_idがないと購入出来ない" do
         @buy.item_id = ""
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Item can't be blank")
+        expect(@buy.errors.full_messages).to include("アイテムを入力してください")
       end
       it "郵便番号が空では購入出来ない" do
         @buy.postal_code = ""
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid")
+        expect(@buy.errors.full_messages).to include("郵便番号を入力してください")
       end  
       it "郵便番号に-がないと購入出来ない" do
         @buy.postal_code = "1111111"
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Postal code is invalid")
+        expect(@buy.errors.full_messages).to include("郵便番号は不正な値です")
       end  
       it "都道府県が---では購入出来ない" do
         @buy.prefecture_id = 1
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@buy.errors.full_messages).to include("都道府県を選んでください")
       end  
       it "市区町村が空では購入出来ない" do
         @buy.municipality = ""
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Municipality can't be blank")
+        expect(@buy.errors.full_messages).to include("市区町村を入力してください")
       end  
       it "番地が空だと購入出来ない" do
         @buy.address = ""
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Address can't be blank")
+        expect(@buy.errors.full_messages).to include("番地を入力してください")
       end  
       it "電話番号が空では購入出来ない" do
         @buy.phone_number = ""
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Phone number can't be blank")
+        expect(@buy.errors.full_messages).to include("電話番号を入力してください")
       end 
       it "電話番号が12ケタ以上では購入出来ない" do
         @buy.phone_number = "111111111111"
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Phone number is invalid")
+        expect(@buy.errors.full_messages).to include("電話番号は不正な値です")
       end  
       it "電話番号が数字のみでないと登録出来ないこと" do
         @buy.phone_number = "111aaa"
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Phone number is invalid")
+        expect(@buy.errors.full_messages).to include("電話番号は不正な値です")
       end
       it "全角数字だと登録出来ないこと" do
         @buy.phone_number = "１１１"
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Phone number is invalid")
+        expect(@buy.errors.full_messages).to include("電話番号は不正な値です")
       end    
       it "tokenが空だと登録出来ない" do
         @buy.token = ""
         @buy.valid?
-        expect(@buy.errors.full_messages).to include("Token can't be blank")
+        expect(@buy.errors.full_messages).to include("クレジットカード情報を入力してください")
       end  
     end  
   
